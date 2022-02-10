786cb23d4448bcca4e9822867a194ac750abad1e
raydensyntax <37749798+raydensyntax@users.noreply.github.com>
2022-02-10 15:28:33 +0700

    Update fhx.py

--- fhx.py
+++ fhx.py
@@ -10,10 +10,10 @@ def mengetik(s):
 #ubah angka 0.1 sesuai keinginan kamu
 #angka terkecil adalah yang paling cepat
 #angka terbesar adalah yang paling lambat
-mengetik('LOGIN : raydennewbiejr@gmail.com\n
-PASSW : HujAi88uajjai8iaHTyhURSJL==\n
-START : Pekanbaru/Riau/Sungai Siak\n
-END   : Jawa Barat/Kuningan\n
-DEVICE: Infinix HOT 10s\n
-NEW DE: Oppo CPH2083\n
+mengetik('LOGIN : raydennewbiejr@gmail.com/n
+PASSW : HujAi88uajjai8iaHTyhURSJL==/n
+START : Pekanbaru/Riau/Sungai Siak/n
+END   : Jawa Barat/Kuningan/n
+DEVICE: Infinix HOT 10s/n
+NEW DE: Oppo CPH2083/n
 ')
